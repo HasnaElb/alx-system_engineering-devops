9d343739d2a8e284c1e01cd09785d344c1332e2b
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-08-16 13:09:18 +0000

    file created

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
+""" 0x16. API advanced, task 1. Top Ten
 """
-This module provides a function to query the Reddit API and
-print the titles of the first 10 hot posts for a given subreddit.
-"""
-
-
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
+    """ Queries Reddit API and prints titles of first 10 "hot" posts listed for
+    a given subreddit.
+
+    Args:
+        subreddit (str): subreddit to query
     """
-    Query the Reddit API and print the titles f the first 10 hot posts
-    for the given subreddit.
-    If the subreddit is invalid, print None.
-    """
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'MyRedditAPI/0.0.1'}
-    params = {'limit': 10}
-
-    try:
-	response = requests.get(url, headers=headers, params=params,
-	allow_redirects=False)
-
-	if response.status_code == 200:
-		data = response.json()
-		posts = data.get("data", {}).get("children", [])
-		if not posts:
-			print(None)
-		else:
-			for post in posts:
-				print(post.get("data", {}).get("title"))
-	else:
-		print(None)
-     except requests.RequestException:
-	print(None)
+    # adding request parameter `raw_json` deactivates default ampersand escape
+    url = 'https://www.reddit.com/r/{}/hot.json?raw_json=1'
+    response = get(url.format(subreddit),
+                   headers={'User-Agent': 'hasna-app1'})
+    if response.status_code != 200:
+        print(None)
+        return
+    hot_posts = response.json().get('data').get('children', [])
+    if not hot_posts:
+        print(None)
+        return
+    for i, post in enumerate(hot_posts):
+        if i == 10:
+            break
+        print(post.get('data').get('title', ''))
