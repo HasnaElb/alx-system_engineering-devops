f1f9f0e247ecbf33f0a02978cc832758cbec9ecf
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-09-07 11:53:55 +0000

    file corrected

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 
 
-from  requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    response = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                   headers={'User-Agent': 'MyRedditBot/1.0'})
-    # non-existent subreddits sometimes return 404
-    if response.status_code != 200:
-        return 0
-    # and sometimes return a dummy JSON dict with only 'Listing' key
-    return response.json().get('data').get('subscribers', 0)
+	#define the URL for the subreddit API
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+	# Define custom headers including User-Agent to avoid getting blocked
+	headers = {'User-Agent': 'my-subreddit-query/0.0.1'}
+
+	# Make the GET request to Reddit's API
+	try:
+		response = requests.get
+		(url, headers=headers, allow_redirects=False)
+
+		# Check if the request was successful
+		if response.status_code == 200:
+			data = response.json()
+			return data['data']['subscribers']
+		else:
+			return 0
+	except requests.RequestException:
+		# In case of any other exceptions, return 0
+		return 0
