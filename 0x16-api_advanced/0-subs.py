b195b14beb28a24edfbef7bc865d6b14b3afef7c
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-09-07 12:44:18 +0000

    file corrected

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
 	# Use .format() for string formatting,
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {'User-Agent': 'my-subreddit-query/0.0.1'}
+	response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+		headers = {'User-Agent': 'my-subreddit-query/0.0.1'})
 
-	try:
-		response = requests.get(url, headers=headers, allow_redirects=False)
-		if response.status_code == 200:
-			data = response.json()
-			return data['data']['subscribers']
-		else:
-			return 0
-	except requests.RequestException:
+	if response.status_code != 200:
 		return 0
+	return response.json().get('data').get('subscribers', 0)
