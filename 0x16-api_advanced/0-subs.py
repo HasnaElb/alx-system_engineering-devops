370bbdd1a3dfa9fb36a1b7b8dc73106190a4344c
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-09-07 12:35:06 +0000

    file corrected

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,19 +5,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    # Use .format() for string formatting,
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        'User-Agent': 'my-subreddit-query/0.0.1'
-    }
+	# Use .format() for string formatting,
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {'User-Agent': 'my-subreddit-query/0.0.1'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
+	try:
+		response = requests.get(url, headers=headers, allow_redirects=False)
+		if response.status_code == 200:
+			data = response.json()
+			return data['data']['subscribers']
+		else:
+			return 0
+	except requests.RequestException:
+		return 0
