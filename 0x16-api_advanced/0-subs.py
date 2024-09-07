fadb1998e83f9698c9e2cb92138d62b7d71a07b1
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-09-07 12:28:59 +0000

    file corrected

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,17 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    # Use .format() for string formatting, since f-strings are not supported in Python 3.4
+    # Use .format() for string formatting,
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    
     headers = {
-        'User-Agent': 'my-subreddit-query/0.0.1'  # Custom User-Agent to avoid 429 error
+        'User-Agent': 'my-subreddit-query/0.0.1'
     }
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
